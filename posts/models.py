cb0018c8e560cf9d8a33a774c061d89d4f13913f
Raymond Rupiah <rupiray@yahoo.com>
2018-04-24 20:59:00 +0100

    added drf

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    #representation of the model should be which is a Django best practice
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# import generics from DRF
+from rest_framework import generics
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
